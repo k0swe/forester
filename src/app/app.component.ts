05adb3c8bae33534b5034dd51cd2505bf3e2c553
Chris Keller <xylo04@gmail.com>
2020-07-03 21:18:14 -0600

    Basic readout of agent websocket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {webSocket} from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'k0s-log';
+  connectedState = "Disconnected"
+  console = '';
+
+  ngOnInit() {
+    const myWebSocket = webSocket('ws://localhost:8081/websocket');
+    this.connectedState = "Connected";
+    myWebSocket.subscribe(
+      msg => this.console += JSON.stringify(msg) + "\n",
+      err => {
+        this.connectedState = "Disconnected";
+        console.log(err);
+      },
+      () => this.connectedState = "Disconnected"
+    );
+  }
 }
