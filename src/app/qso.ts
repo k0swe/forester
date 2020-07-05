faa6c30251e36393eb37f91d543c6c76f97a18ba
Chris Keller <xylo04@gmail.com>
2020-07-05 07:49:43 -0600

    Add table fields and apply bootstrap styling

--- src/app/qso.ts
+++ src/app/qso.ts
@@ -1,13 +1,17 @@
 export interface Qso {
   band: string;
+  country: string;
   dxCall: string;
   dxGrid: string;
   frequencyHz: number;
   mode: string;
   myCall: string;
   myGrid: string;
+  name: string;
+  qth: string
   rstReceived: string;
   rstSent: string;
+  state: string;
   timeOff: Date;
   timeOn: Date;
 }
