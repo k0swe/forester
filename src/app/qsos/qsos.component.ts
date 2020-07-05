9dee49e32ce744874e2861b19d06a228c7ef00ed
Chris Keller <xylo04@gmail.com>
2020-07-04 22:11:51 -0600

    View multiple QSOs

--- src/app/qsos/qsos.component.ts
+++ src/app/qsos/qsos.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Qso} from "../qso";
+import {Qso} from '../qso';
 
 @Component({
   selector: 'app-qsos',
@@ -7,19 +7,33 @@ import {Qso} from "../qso";
   styleUrls: ['./qsos.component.css']
 })
 export class QsosComponent implements OnInit {
-  qso: Qso = {
-    band: "20m",
-    dxCall: "N0CALL",
-    dxGrid: "ZM33",
-    frequencyHz: 14074000,
-    mode: "FT8",
-    myCall: "K0SWE",
-    myGrid: "DM79",
-    rstReceived: "",
-    rstSent: "",
-    timeOff: new Date(),
-    timeOn: new Date(),
-  }
+  qsos: Qso[] = [
+    {
+      band: '20m',
+      dxCall: 'N0CALL',
+      dxGrid: 'ZM33',
+      frequencyHz: 14074000,
+      mode: 'FT8',
+      myCall: 'K0SWE',
+      myGrid: 'DM79',
+      rstReceived: '',
+      rstSent: '',
+      timeOff: new Date(),
+      timeOn: new Date(),
+    }, {
+      band: '40m',
+      dxCall: 'T3ST',
+      dxGrid: 'YD56',
+      frequencyHz: 7074000,
+      mode: 'FT8',
+      myCall: 'K0SWE',
+      myGrid: 'DM79',
+      rstReceived: '',
+      rstSent: '',
+      timeOff: new Date(),
+      timeOn: new Date(),
+    }
+  ];
 
   constructor() {
   }
