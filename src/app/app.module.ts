dbf1c616dd9342ac96fd48d0d3cff8bafa62670e
Chris Keller <xylo04@gmail.com>
2020-07-17 19:57:13 -0600

    Basic angular material treatment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+import {AgentComponent} from './agent/agent.component';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {HomeComponent} from './home/home.component';
+import {HttpClientModule} from "@angular/common/http";
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
 import {QsoListComponent} from './qso-list/qso-list.component';
 import {QsoService} from './shared/qso.service';
-import {HttpClientModule} from "@angular/common/http";
-import {HomeComponent} from './home/home.component';
-import {AgentComponent} from './agent/agent.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
@@ -19,9 +23,13 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule,
-    HttpClientModule,
     BrowserAnimationsModule,
+    BrowserModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
   ],
   providers: [QsoService],
   bootstrap: [AppComponent]
