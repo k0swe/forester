c4adc9efca6a5cf6baebcee95dbd592650129811
Chris Keller <xylo04@gmail.com>
2020-08-18 17:42:32 -0600

    Basic read from firestore, really dumb

--- src/app/shared/qso.service.ts
+++ src/app/shared/qso.service.ts
@@ -1,26 +1,58 @@
-import {HttpClient} from "@angular/common/http";
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {Qso} from "../qso";
-import {Adif} from "../../generated/adif_pb";
-import {map} from "rxjs/operators";
+import {Qso as QsoPb} from "../../generated/adif_pb";
+import {map, switchMap} from "rxjs/operators";
+import {AngularFirestore} from "@angular/fire/firestore";
+import {AuthService} from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class QsoService {
-  constructor(private http: HttpClient) {
+  qsos$: Observable<Qso[]>;
+
+  constructor(
+    private authService: AuthService,
+    private firestore: AngularFirestore) {
+    const uid$ = authService.user().pipe(map(user => user.uid));
+    this.qsos$ = uid$.pipe(switchMap(userId =>
+      this.firestore
+        .collection("contacts", ref => ref.where('owner', '==', userId))
+        .valueChanges()
+        .pipe(map(docs => this.unpackDocs(docs)))
+    ));
   }
 
   getQsos(): Observable<Qso[]> {
-    return this.http.get<Adif.AsObject>("/assets/test.json")
-      .pipe(map(adif => {
-        const qsos = new Array<Qso>();
-        adif.qsos.forEach(
-          // parse ADIF JSON into internal DTO
-          qso => qsos.push(Qso.fromObject(qso))
-        );
-        return qsos;
-      }));
+    return this.qsos$;
   }
+
+  private unpackDocs(docs: unknown[]): Qso[] {
+    return docs.map(
+      doc => Qso.fromObject((doc as FsQso).qso));
+  }
+
+  private insert(qso: QsoPb.AsObject) {
+    this.authService.user().subscribe(user => {
+        if (user == null) {
+          return;
+        }
+        const fsQso: FsQso = {
+          qso: qso,
+          owner: user.uid
+        };
+        this.firestore
+          .collection("contacts")
+          .add(fsQso);
+      }
+    );
+  }
+}
+
+// Firestore Qso
+interface FsQso {
+  qso: QsoPb.AsObject
+  // Firebase user UID
+  owner: string
 }
