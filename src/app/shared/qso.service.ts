034d2002e75e9df2b818cb1d5ae8df3093d2c63a
Chris Keller <xylo04@gmail.com>
2020-08-18 22:43:31 -0600

    Restructure db

--- src/app/shared/qso.service.ts
+++ src/app/shared/qso.service.ts
@@ -1,10 +1,10 @@
+import {AngularFirestore} from '@angular/fire/firestore';
+import {AuthService} from './auth.service';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
-import {Qso} from "../qso";
-import {Qso as QsoPb} from "../../generated/adif_pb";
-import {map, switchMap} from "rxjs/operators";
-import {AngularFirestore} from "@angular/fire/firestore";
-import {AuthService} from "./auth.service";
+import {Qso as QsoPb} from '../../generated/adif_pb';
+import {Qso} from '../qso';
+import {map, switchMap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,43 +16,21 @@ export class QsoService {
     private authService: AuthService,
     private firestore: AngularFirestore) {
     const uid$ = authService.user().pipe(map(user => user.uid));
-    this.qsos$ = uid$.pipe(switchMap(userId =>
-      this.firestore
-        .collection("contacts", ref => ref.where('owner', '==', userId))
+    this.qsos$ = uid$.pipe(switchMap(userId => {
+      const userDoc = firestore.doc('users/' + userId);
+      return userDoc
+        .collection<QsoPb.AsObject>('contacts')
         .valueChanges()
-        .pipe(map(docs => this.unpackDocs(docs)))
-    ));
+        .pipe(map(qsos => this.unpackDocs(qsos)));
+    }));
   }
 
   getQsos(): Observable<Qso[]> {
     return this.qsos$;
   }
 
-  private unpackDocs(docs: unknown[]): Qso[] {
-    return docs.map(
-      doc => Qso.fromObject((doc as FsQso).qso));
+  private unpackDocs(pbQsos: QsoPb.AsObject[]): Qso[] {
+    return pbQsos.map(
+      pbQso => Qso.fromObject(pbQso));
   }
-
-  private insert(qso: QsoPb.AsObject) {
-    this.authService.user().subscribe(user => {
-        if (user == null) {
-          return;
-        }
-        const fsQso: FsQso = {
-          qso: qso,
-          owner: user.uid
-        };
-        this.firestore
-          .collection("contacts")
-          .add(fsQso);
-      }
-    );
-  }
-}
-
-// Firestore Qso
-interface FsQso {
-  qso: QsoPb.AsObject
-  // Firebase user UID
-  owner: string
 }
