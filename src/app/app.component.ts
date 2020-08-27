2b209a1444b20ed5b72839297580a44d3d3f09f5
Chris Keller <xylo04@gmail.com>
2020-08-26 21:04:04 -0600

    Simple button and logic for Import cloud func

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {AuthService} from './shared/auth.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'kel-root',
@@ -6,4 +8,25 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  qrzImportUrl: string;
+  qrzImportUrlBase = 'https://us-central1-k0swe-kellog.cloudfunctions.net/HelloHTTP';
+
+  constructor(public authService: AuthService, private http: HttpClient) {
+  }
+
+  importFromQrz(): void {
+    this.authService.user().subscribe(
+      user => {
+        if (user != null) {
+          user.getIdToken(false).then(token => {
+            this.qrzImportUrl = this.qrzImportUrlBase + '?token=' + token;
+            console.log('QRZ import url is', this.qrzImportUrl);
+            this.http.get(this.qrzImportUrl).subscribe(response => {
+              console.log(response);
+            });
+          });
+        }
+      }
+    );
+  }
 }
