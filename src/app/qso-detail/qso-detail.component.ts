f7b29086aaf4ec331b5214703ac63bc7a532de32
Chris Keller <xylo04@gmail.com>
2020-09-06 17:33:51 -0600

    Restructure QSO details

--- src/app/qso-detail/qso-detail.component.ts
+++ src/app/qso-detail/qso-detail.component.ts
@@ -1,6 +1,8 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {Qso} from '../qso';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {DatePipe} from '@angular/common';
 
 export interface QsoDetailData {
   qso: Qso;
@@ -9,14 +11,24 @@ export interface QsoDetailData {
 @Component({
   selector: 'kel-qso-detail',
   templateUrl: './qso-detail.component.html',
-  styleUrls: ['./qso-detail.component.css']
+  styleUrls: ['./qso-detail.component.scss'],
+  providers: [DatePipe],
 })
-export class QsoDetailComponent implements OnInit {
+export class QsoDetailComponent {
+  qsoDetailForm: FormGroup;
 
-  constructor(public dialogRef: MatDialogRef<QsoDetailComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: QsoDetailData) {
-  }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder,
+              @Inject(MAT_DIALOG_DATA) public data: QsoDetailData,
+              private datePipe: DatePipe) {
+    this.qsoDetailForm = fb.group({
+      timeOn: this.datePipe.transform(data.qso.timeOn, 'yyyy-MM-dd HH:mm', 'UTC'),
+      timeOff: this.datePipe.transform(data.qso.timeOff, 'yyyy-MM-dd HH:mm', 'UTC'),
+      callsign: data.qso.contactedCall,
+      band: data.qso.band,
+      freq: data.qso.freq,
+      mode: data.qso.mode,
+      rstSent: data.qso.rstSent,
+      rstRcvd: data.qso.rstReceived,
+    });
   }
 }
