0072a3fe440fcd8fd7fef21dacdfd90aac612d5a
Chris Keller <xylo04@gmail.com>
2020-09-30 02:49:31 -0600

    Update deprecated call

--- src/app/agent/agent.component.spec.ts
+++ src/app/agent/agent.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import {AgentComponent} from './agent.component';
 
@@ -6,7 +6,7 @@ describe('AgentComponent', () => {
   let component: AgentComponent;
   let fixture: ComponentFixture<AgentComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [AgentComponent]
     })
