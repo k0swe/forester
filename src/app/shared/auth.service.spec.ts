7b1192e03586a883194f03bb20fbfc8906cf0e17
Chris Keller <xylo04@gmail.com>
2020-09-30 03:05:27 -0600

    Fix auth service spec

--- src/app/shared/auth.service.spec.ts
+++ src/app/shared/auth.service.spec.ts
@@ -1,12 +1,20 @@
-import {TestBed} from '@angular/core/testing';
-
+import {AngularFireAuth} from '@angular/fire/auth';
 import {AuthService} from './auth.service';
+import {TestBed} from '@angular/core/testing';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: AngularFireAuth, useValue: {
+            signInWithPopup: () => null
+          }
+        },
+      ]
+    });
     service = TestBed.inject(AuthService);
   });
 
