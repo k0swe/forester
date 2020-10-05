fa3a953af334473c5921507f083d0286b22745ec
Chris Keller <xylo04@gmail.com>
2020-10-05 15:39:37 -0600

    Rename css file

--- src/app/avatar/avatar.component.ts
+++ src/app/avatar/avatar.component.ts
@@ -4,7 +4,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'kel-avatar',
   templateUrl: './avatar.component.html',
-  styleUrls: ['./avatar.component.css']
+  styleUrls: ['./avatar.component.scss']
 })
 export class AvatarComponent {
 
