b6ae9ae0b5e384ffc10b83c0f6371904ad743aa5
Chris Keller <xylo04@gmail.com>
2020-10-23 11:56:42 -0600

    Start parsing imported ADIF

--- src/app/shared/import-export.service.ts
+++ src/app/shared/import-export.service.ts
@@ -1,18 +1,29 @@
+import { AdifParser } from 'adif-parser-ts';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { QsoService } from './qso.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ImportExportService {
-  constructor(private qsoService: QsoService) {}
+  constructor(private qsoService: QsoService, private snackBar: MatSnackBar) {}
 
   public importAdi(file: File): void {
     const fileReader = new FileReader();
     fileReader.onload = () => {
       console.log('Imported', file.name, file.type, file.size);
       const content = fileReader.result as string;
-      console.log(content);
+      try {
+        console.log(AdifParser.parseAdi(content));
+      } catch (e) {
+        this.snackBar.open(
+          'There was a problem importing the ADIF file',
+          null,
+          { duration: 10000 }
+        );
+        console.log('There was a problem importing the ADIF file\n', e);
+      }
     };
     fileReader.readAsText(file);
   }
