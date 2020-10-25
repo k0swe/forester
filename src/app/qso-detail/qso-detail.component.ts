ae233242cde7abb68762c5f7d21314fb3c5845ec
Chris Keller <xylo04@gmail.com>
2020-10-25 16:05:40 -0600

    Add grid square to QSO details display

--- src/app/qso-detail/qso-detail.component.ts
+++ src/app/qso-detail/qso-detail.component.ts
@@ -36,6 +36,7 @@ export class QsoDetailComponent {
       state: undefined,
       country: undefined,
       continent: undefined,
+      gridSquare: undefined,
     },
   };
   private readonly firebaseId;
