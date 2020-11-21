e6140184cb4255c5f905bfd7f585acb96d5c5be0
Chris Keller <xylo04@gmail.com>
2020-11-21 15:09:14 -0700

    Fix ImportExportService spec

--- src/app/shared/import-export.service.spec.ts
+++ src/app/shared/import-export.service.spec.ts
@@ -1,12 +1,30 @@
-import { TestBed } from '@angular/core/testing';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { ImportExportService } from './import-export.service';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { TestBed } from '@angular/core/testing';
 
 describe('ImportExportService', () => {
   let service: ImportExportService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [MatSnackBarModule],
+      providers: [
+        {
+          provide: AngularFireAuth,
+          useValue: {
+            signInWithPopup: () => null,
+          },
+        },
+        {
+          provide: AngularFirestore,
+          useValue: {
+            doc: () => null,
+          },
+        },
+      ],
+    });
     service = TestBed.inject(ImportExportService);
   });
 
