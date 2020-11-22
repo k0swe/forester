56c9ef9f6359aa5bdb563e1dec016afde6bdaf6b
Chris Keller <xylo04@gmail.com>
2020-11-21 17:35:05 -0700

    Fix AgentService spec

--- src/app/shared/agent.service.spec.ts
+++ src/app/shared/agent.service.spec.ts
@@ -1,11 +1,28 @@
 import { AgentService } from './agent.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { TestBed } from '@angular/core/testing';
 
 describe('AgentService', () => {
   let service: AgentService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: AngularFireAuth,
+          useValue: {
+            signInWithPopup: () => null,
+          },
+        },
+        {
+          provide: AngularFirestore,
+          useValue: {
+            doc: () => null,
+          },
+        },
+      ],
+    });
     service = TestBed.inject(AgentService);
   });
 
