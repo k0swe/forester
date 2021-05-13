b21cce93bdac3ccb3ad793e12eb2dd246c29f59c
Chris Keller <xylo04@gmail.com>
2021-05-13 16:33:52 -0600

    Dynamically generate license info

--- src/app/pages/credits/credits.component.ts
+++ src/app/pages/credits/credits.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'kel-credits',
@@ -6,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./credits.component.scss'],
 })
 export class CreditsComponent implements OnInit {
-  constructor() {}
+  licenses = new BehaviorSubject<Array<LicenseInfo>>([]);
+  columnsToDisplay = ['name', 'installedVersion', 'author', 'licenseType'];
 
-  ngOnInit(): void {}
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<Array<LicenseInfo>>('/assets/license-report.json')
+      .subscribe((response) => {
+        response = response.map((lic) => {
+          lic.link = lic.link.replace('git+', '');
+          lic.link = lic.link.replace('.git', '');
+          lic.link = lic.link.replace('git://', 'https://');
+          return lic;
+        });
+        this.licenses.next(response);
+      });
+  }
+}
+
+interface LicenseInfo {
+  name: string;
+  licenseType: string;
+  link: string;
+  remoteVersion: string;
+  installedVersion: string;
+  author?: string;
 }
