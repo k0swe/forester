341240645a3bd49570d8d24fa192e14cf7eed3a9
Chris Keller <xylo04@gmail.com>
2021-09-09 13:54:40 -0600

    Prevent null from being exported in ADI

--- src/app/shared/proto2adif.ts
+++ src/app/shared/proto2adif.ts
@@ -55,24 +55,24 @@ export class Proto2Adif {
     qso: Qso,
     record: { [p: string]: string }
   ): void {
-    record.band = qso.band;
-    record.band_rx = qso.bandRx;
-    record.comment = qso.comment ? qso.comment : undefined;
+    record.band = this.getString(qso.band);
+    record.band_rx = this.getString(qso.bandRx);
+    record.comment = this.getString(qso.comment);
     record.distance = this.getNumber(qso.distanceKm);
     record.freq = this.getNumber(qso.freq);
     record.freq_rx = this.getNumber(qso.freqRx);
-    record.mode = qso.mode;
-    record.notes = qso.notes ? qso.notes : undefined;
-    record.public_key = qso.publicKey;
-    record.qso_complete = qso.complete;
+    record.mode = this.getString(qso.mode);
+    record.notes = this.getString(qso.notes);
+    record.public_key = this.getString(qso.publicKey);
+    record.qso_complete = this.getString(qso.complete);
     record.qso_date = this.getDate(qso.timeOn);
     record.time_on = this.getTime(qso.timeOn);
     record.qso_date_off = this.getDate(qso.timeOff);
     record.time_off = this.getTime(qso.timeOff);
     record.qso_random = this.getBool(qso.random);
-    record.rst_rcvd = qso.rstReceived;
-    record.rst_sent = qso.rstSent;
-    record.submode = qso.submode;
+    record.rst_rcvd = this.getString(qso.rstReceived);
+    record.rst_sent = this.getString(qso.rstSent);
+    record.submode = this.getString(qso.submode);
     record.swl = this.getBool(qso.swl);
   }
 
@@ -94,32 +94,32 @@ export class Proto2Adif {
   ): void {
     record.ant_az = this.getNumber(loggingStation.antennaAzimuth);
     record.ant_el = this.getNumber(loggingStation.antennaElevation);
-    record.my_antenna = loggingStation.antenna;
-    record.my_city = loggingStation.city;
-    record.my_cnty = loggingStation.county;
-    record.my_country = loggingStation.country;
+    record.my_antenna = this.getString(loggingStation.antenna);
+    record.my_city = this.getString(loggingStation.city);
+    record.my_cnty = this.getString(loggingStation.county);
+    record.my_country = this.getString(loggingStation.country);
     record.my_cq_zone = this.getNumber(loggingStation.cqZone);
     record.my_dxcc = this.getNumber(loggingStation.dxcc);
     record.my_fists = this.getNumber(loggingStation.fists);
-    record.my_gridsquare = loggingStation.gridSquare;
-    record.my_iota = loggingStation.iota;
+    record.my_gridsquare = this.getString(loggingStation.gridSquare);
+    record.my_iota = this.getString(loggingStation.iota);
     record.my_iota_island_id = this.getNumber(loggingStation.iotaIslandId);
     record.my_itu_zone = this.getNumber(loggingStation.ituZone);
     record.my_lat = this.getLatLon(loggingStation.latitude, true);
     record.my_lon = this.getLatLon(loggingStation.longitude, false);
-    record.my_name = loggingStation.opName;
-    record.my_postal_code = loggingStation.postalCode;
-    record.my_rig = loggingStation.rig;
-    record.my_sig = loggingStation.sig;
-    record.my_sig_info = loggingStation.sigInfo;
-    record.my_sota_ref = loggingStation.sotaRef;
-    record.my_state = loggingStation.state;
-    record.my_street = loggingStation.street;
-    record.my_usaca_counties = loggingStation.usacaCounties;
-    record.my_vucc_grids = loggingStation.vuccGrids;
-    record.operator = loggingStation.opCall;
-    record.owner_callsign = loggingStation.ownerCall;
-    record.station_callsign = loggingStation.stationCall;
+    record.my_name = this.getString(loggingStation.opName);
+    record.my_postal_code = this.getString(loggingStation.postalCode);
+    record.my_rig = this.getString(loggingStation.rig);
+    record.my_sig = this.getString(loggingStation.sig);
+    record.my_sig_info = this.getString(loggingStation.sigInfo);
+    record.my_sota_ref = this.getString(loggingStation.sotaRef);
+    record.my_state = this.getString(loggingStation.state);
+    record.my_street = this.getString(loggingStation.street);
+    record.my_usaca_counties = this.getString(loggingStation.usacaCounties);
+    record.my_vucc_grids = this.getString(loggingStation.vuccGrids);
+    record.operator = this.getString(loggingStation.opCall);
+    record.owner_callsign = this.getString(loggingStation.ownerCall);
+    record.station_callsign = this.getString(loggingStation.stationCall);
     record.tx_pwr = this.getNumber(loggingStation.power);
   }
 
@@ -129,42 +129,42 @@ export class Proto2Adif {
   ): void {
     record.address = contactedStation.address;
     record.age = this.getNumber(contactedStation.age);
-    record.call = contactedStation.stationCall;
-    record.cnty = contactedStation.county;
-    record.cont = contactedStation.continent;
-    record.contacted_op = contactedStation.opCall;
-    record.country = contactedStation.country;
+    record.call = this.getString(contactedStation.stationCall);
+    record.cnty = this.getString(contactedStation.county);
+    record.cont = this.getString(contactedStation.continent);
+    record.contacted_op = this.getString(contactedStation.opCall);
+    record.country = this.getString(contactedStation.country);
     record.cqz = this.getNumber(contactedStation.cqZone);
-    record.darc_dok = contactedStation.darcDok;
+    record.darc_dok = this.getString(contactedStation.darcDok);
     record.dxcc = this.getNumber(contactedStation.dxcc);
-    record.email = contactedStation.email;
-    record.eq_call = contactedStation.ownerCall;
+    record.email = this.getString(contactedStation.email);
+    record.eq_call = this.getString(contactedStation.ownerCall);
     record.fists = this.getNumber(contactedStation.fists);
     record.fists_cc = this.getNumber(contactedStation.fistsCc);
-    record.gridsquare = contactedStation.gridSquare;
-    record.iota = contactedStation.iota;
+    record.gridsquare = this.getString(contactedStation.gridSquare);
+    record.iota = this.getString(contactedStation.iota);
     record.iota_island_id = this.getNumber(contactedStation.iotaIslandId);
     record.ituz = this.getNumber(contactedStation.ituZone);
     record.lat = this.getLatLon(contactedStation.latitude, true);
     record.lon = this.getLatLon(contactedStation.longitude, false);
-    record.name = contactedStation.opName;
-    record.pfx = contactedStation.pfx;
-    record.qsl_via = contactedStation.qslVia;
-    record.qth = contactedStation.city;
-    record.region = contactedStation.region;
-    record.rig = contactedStation.rig;
+    record.name = this.getString(contactedStation.opName);
+    record.pfx = this.getString(contactedStation.pfx);
+    record.qsl_via = this.getString(contactedStation.qslVia);
+    record.qth = this.getString(contactedStation.city);
+    record.region = this.getString(contactedStation.region);
+    record.rig = this.getString(contactedStation.rig);
     record.rx_pwr = this.getNumber(contactedStation.power);
-    record.sig = contactedStation.sig;
-    record.sig_info = contactedStation.sigInfo;
+    record.sig = this.getString(contactedStation.sig);
+    record.sig_info = this.getString(contactedStation.sigInfo);
     record.silent_key = this.getBool(contactedStation.silentKey);
-    record.skcc = contactedStation.skcc;
-    record.sota_ref = contactedStation.sotaRef;
-    record.state = contactedStation.state;
+    record.skcc = this.getString(contactedStation.skcc);
+    record.sota_ref = this.getString(contactedStation.sotaRef);
+    record.state = this.getString(contactedStation.state);
     record.ten_ten = this.getNumber(contactedStation.tenTen);
     record.uksmg = this.getNumber(contactedStation.uksmg);
-    record.usaca_counties = contactedStation.usacaCounties;
-    record.vucc_grids = contactedStation.vuccGrids;
-    record.web = contactedStation.web;
+    record.usaca_counties = this.getString(contactedStation.usacaCounties);
+    record.vucc_grids = this.getString(contactedStation.vuccGrids);
+    record.web = this.getString(contactedStation.web);
   }
 
   private static translateContest(
@@ -174,13 +174,13 @@ export class Proto2Adif {
     if (!contest) {
       return;
     }
-    record.contest_id = contest.contestId;
-    record.arrl_sect = contest.arrlSection;
-    record.class = contest.stationClass;
-    record.check = contest.check;
-    record.precedence = contest.precedence;
-    record.srx_string = contest.serialReceived;
-    record.stx_string = contest.serialSent;
+    record.contest_id = this.getString(contest.contestId);
+    record.arrl_sect = this.getString(contest.arrlSection);
+    record.class = this.getString(contest.stationClass);
+    record.check = this.getString(contest.check);
+    record.precedence = this.getString(contest.precedence);
+    record.srx_string = this.getString(contest.serialReceived);
+    record.stx_string = this.getString(contest.serialSent);
   }
 
   private static translatePropagation(
@@ -191,16 +191,16 @@ export class Proto2Adif {
       return;
     }
     record.a_index = this.getNumber(prop.aIndex);
-    record.ant_path = prop.antPath;
+    record.ant_path = this.getString(prop.antPath);
     record.force_init = this.getBool(prop.forceInit);
     record.k_index = this.getNumber(prop.kIndex);
     record.max_bursts = this.getNumber(prop.maxBursts);
-    record.ms_shower = prop.meteorShowerName;
+    record.ms_shower = this.getString(prop.meteorShowerName);
     record.nr_bursts = this.getNumber(prop.nrBursts);
     record.nr_pings = this.getNumber(prop.nrPings);
-    record.prop_mode = prop.propagationMode;
-    record.sat_mode = prop.satMode;
-    record.sat_name = prop.satName;
+    record.prop_mode = this.getString(prop.propagationMode);
+    record.sat_mode = this.getString(prop.satMode);
+    record.sat_name = this.getString(prop.satName);
     record.sfi = this.getNumber(prop.solarFluxIndex);
   }
 
@@ -291,9 +291,9 @@ export class Proto2Adif {
       return undefined;
     }
     this.translateQsl(qsl, '', record);
-    record.qsl_sent_via = qsl.sentVia;
-    record.qsl_rcvd_via = qsl.receivedVia;
-    record.qslmsg = qsl.receivedMessage;
+    record.qsl_sent_via = this.getString(qsl.sentVia);
+    record.qsl_rcvd_via = this.getString(qsl.receivedVia);
+    record.qslmsg = this.getString(qsl.receivedMessage);
   }
 
   private static translateQsl(
@@ -304,17 +304,18 @@ export class Proto2Adif {
     if (!qsl) {
       return;
     }
-    record[qslProvider + 'qsl_sent'] = qsl.sentStatus;
-    record[qslProvider + 'qsl_rcvd'] = qsl.receivedStatus;
+    record[qslProvider + 'qsl_sent'] = this.getString(qsl.sentStatus);
+    record[qslProvider + 'qsl_rcvd'] = this.getString(qsl.receivedStatus);
     record[qslProvider + 'qslsdate'] = this.getDate(qsl.sentDate);
     record[qslProvider + 'qslrdate'] = this.getDate(qsl.receivedDate);
   }
 
+  private static getString(str: string): string | undefined {
+    return !!str ? str : undefined;
+  }
+
   private static getNumber(num: number): string | undefined {
-    if (!num) {
-      return undefined;
-    }
-    return num.toString(10);
+    return num ? num.toString(10) : undefined;
   }
 
   private static getBool(bool: boolean): string | undefined {
