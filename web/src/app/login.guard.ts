db36415f73d101423ee4485e3236ed37018a88c0
Chris Keller <xylo04@gmail.com>
2023-06-06 09:16:57 -0600

    Upgrade to Angular 16

--- web/src/app/login.guard.ts
+++ web/src/app/login.guard.ts
@@ -1,10 +1,4 @@
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { AuthService } from './shared/auth/auth.service';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
@@ -13,7 +7,7 @@ import { switchMap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
-export class LoginGuard implements CanActivate {
+export class LoginGuard  {
   constructor(private router: Router, private authService: AuthService) {}
 
   canActivate(
