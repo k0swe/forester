479f7a4f8446305b35a89a765916331736d4a819
Chris Keller <xylo04@gmail.com>
2023-11-07 14:42:28 -0700

    Add address geocoding (#2207)
    * WIP: geocode station addresses
    
    * Paste "lat, long" in station details
    
    * If a station has lat/long, use that for geocoding city/county/state/country

--- web/src/app/shared/station-detail/station-detail.component.ts
+++ web/src/app/shared/station-detail/station-detail.component.ts
@@ -5,6 +5,7 @@ import {
   OnChanges,
   Output,
   SimpleChanges,
+  inject,
 } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
 import isEqual from 'lodash/isEqual';
@@ -13,9 +14,10 @@ import { map } from 'rxjs/operators';
 
 import { Station } from '../../qso';
 import { DxccRef } from '../../reference/dxcc';
+import { GeocodeService } from '../geocode/geocode.service';
 import { StationLocationValidator } from './station-location-validator';
 
-const googleMapsSearchBase = 'https://www.google.com/maps/search/';
+const googleMapsSearchBase = new URL('https://www.google.com/maps/search/');
 
 @Component({
   selector: 'kel-station-detail',
@@ -27,6 +29,7 @@ export class StationDetailComponent implements OnChanges {
   @Output() stationChange = new EventEmitter<Station>();
   @Input() showRig: boolean = true;
   @Input() showTxPower: boolean = true;
+  private geocodeService = inject(GeocodeService);
   stationDetailForm: FormGroup;
   countries = DxccRef.getNames();
   filteredCountries$: Observable<string[]>;
@@ -40,6 +43,7 @@ export class StationDetailComponent implements OnChanges {
     latitude: undefined,
     longitude: undefined,
     city: undefined,
+    county: undefined,
     state: undefined,
     country: undefined,
     dxcc: undefined,
@@ -132,7 +136,8 @@ export class StationDetailComponent implements OnChanges {
     const state: string = this.stationDetailForm.get('state').value;
     const country: string = this.stationDetailForm.get('country').value;
     if (latitude && longitude) {
-      this.mapLink = googleMapsSearchBase + latitude + ',' + longitude;
+      this.mapLink =
+        googleMapsSearchBase.toString() + latitude + ',' + longitude;
     } else if (city || state || country) {
       this.mapLink =
         googleMapsSearchBase +
@@ -143,6 +148,32 @@ export class StationDetailComponent implements OnChanges {
         encodeURIComponent(country);
     } else {
       this.mapLink = '';
+    }
+  }
+
+  geocode() {
+    this.geocodeService
+      .geocode(this.stationDetailForm.value)
+      .then((station) => {
+        this.stationDetailForm.patchValue(station);
+      });
+  }
+
+  pasteLatOrLon($event: ClipboardEvent) {
+    if ($event.clipboardData.getData('text/plain').includes(',')) {
+      // might be "latitude, longitude"
+      const [latStr, lonStr] = $event.clipboardData
+        .getData('text/plain')
+        .split(',');
+      const latitude = Number(latStr);
+      const longitude = Number(lonStr);
+      if (!isNaN(latitude) && !isNaN(longitude)) {
+        this.stationDetailForm.patchValue({
+          latitude: latitude.toFixed(4),
+          longitude: longitude.toFixed(4),
+        });
+        $event.preventDefault();
+      }
     }
   }
 }
