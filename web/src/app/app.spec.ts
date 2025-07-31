d3436e5b2c1b7bf088e2f878ce62c2d9cdee3b2e
Chris Keller <xylo04@gmail.com>
2025-07-31 16:33:22 -0600

    Formatting

--- web/src/app/app.spec.ts
+++ web/src/app/app.spec.ts
@@ -18,6 +18,8 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, forester');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, forester',
+    );
   });
 });
--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('forester');
