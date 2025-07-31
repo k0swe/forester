50b497a9b4d2f9ff86b1741b65a76f9ba6aeba33
Chris Keller <xylo04@gmail.com>
2025-07-31 17:57:06 +0000

    prettier

--- web/karma.conf.js
+++ web/karma.conf.js
@@ -10,7 +10,6 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      
     ],
     files: ['./src/test/google-maps-mock.js'],
     client: {
