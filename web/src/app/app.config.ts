55f043e7b52545d86b7e9fc20ed45c306738cd7d
Chris Keller <xylo04@gmail.com>
2025-08-02 10:51:17 -0600

    Add firebase auth providers

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -6,11 +6,16 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAuth(() => getAuth()),
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }),
   ],
 };
