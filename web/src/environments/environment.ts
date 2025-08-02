3abdbfb1b81e55a86ca7a1479ef4c523bb76da5b
Chris Keller <xylo04@gmail.com>
2025-08-02 10:49:42 -0600

    Copy over environment files

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,1 +1,15 @@
-export const environment = {};
+// noinspection SpellCheckingInspection
+export const environment = {
+  production: true,
+  functionsBase: 'https://us-central1-k0swe-kellog.cloudfunctions.net/',
+  firebase: {
+    apiKey: 'AIzaSyCaOdT7P0gex-Wb1ga5meShIX_cDL64WF8',
+    authDomain: 'k0swe-kellog.firebaseapp.com',
+    databaseURL: 'https://k0swe-kellog.firebaseio.com',
+    projectId: 'k0swe-kellog',
+    storageBucket: 'k0swe-kellog.appspot.com',
+    messagingSenderId: '1062132260085',
+    appId: '1:1062132260085:web:241eb522b9821edf0239bc',
+    measurementId: 'G-EYXSXWYN0P',
+  },
+};
