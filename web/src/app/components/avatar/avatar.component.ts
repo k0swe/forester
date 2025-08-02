d7bbc5615afb09a76ae6bdad599fc54c0c7f4839
Chris Keller <xylo04@gmail.com>
2025-08-02 10:52:28 -0600

    Generate avatar component

---
+++ web/src/app/components/avatar/avatar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar',
+  imports: [],
+  templateUrl: './avatar.component.html',
+  styleUrl: './avatar.component.scss',
+})
+export class AvatarComponent {}
