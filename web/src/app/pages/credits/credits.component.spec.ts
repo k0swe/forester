3516a5cf909060cbcd21c0069a37733b0c6a2bbe
Chris Keller <xylo04@gmail.com>
2025-08-03 10:37:35 -0600

    Replace deprecated

--- web/src/app/pages/credits/credits.component.spec.ts
+++ web/src/app/pages/credits/credits.component.spec.ts
@@ -1,4 +1,5 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { CreditsComponent } from './credits.component';
@@ -6,8 +7,8 @@ import { CreditsComponent } from './credits.component';
 describe('CreditsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CreditsComponent, HttpClientTestingModule],
-      providers: [],
+      imports: [CreditsComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
   });
 
