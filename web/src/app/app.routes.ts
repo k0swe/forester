24d16fc70da2ef6dc4af1453e7809ba072b2844b
Chris Keller <xylo04@gmail.com>
2025-08-10 14:40:27 -0600

    Use lazy-loaded routes

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,40 +1,57 @@
 import { Routes } from '@angular/router';
 
 import { LoginGuard } from './login.guard';
-import { CreditsComponent } from './pages/credits/credits.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LogbookComponent } from './pages/logbook/logbook.component';
-import { MapComponent } from './pages/map/map.component';
-import { PrivacyAndTermsComponent } from './pages/privacy-and-terms/privacy-and-terms.component';
-import { QsoListComponent } from './pages/qso-list/qso-list.component';
-import { WasComponent } from './pages/was/was.component';
-import { LoginComponent } from './shared/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
   },
   {
     path: 'credits',
-    component: CreditsComponent,
+    loadComponent: () =>
+      import('./pages/credits/credits.component').then(
+        (m) => m.CreditsComponent,
+      ),
   },
   {
     path: 'privacy-and-terms',
-    component: PrivacyAndTermsComponent,
+    loadComponent: () =>
+      import('./pages/privacy-and-terms/privacy-and-terms.component').then(
+        (m) => m.PrivacyAndTermsComponent,
+      ),
   },
   {
     path: 'login',
-    component: LoginComponent,
+    loadComponent: () =>
+      import('./shared/login/login.component').then((m) => m.LoginComponent),
   },
   {
     path: ':callsign',
-    component: LogbookComponent,
+    loadComponent: () =>
+      import('./pages/logbook/logbook.component').then(
+        (m) => m.LogbookComponent,
+      ),
     canActivate: [LoginGuard],
     children: [
-      { path: 'qsos', component: QsoListComponent },
-      { path: 'map', component: MapComponent },
-      { path: 'was', component: WasComponent },
+      {
+        path: 'qsos',
+        loadComponent: () =>
+          import('./pages/qso-list/qso-list.component').then(
+            (m) => m.QsoListComponent,
+          ),
+      },
+      {
+        path: 'map',
+        loadComponent: () =>
+          import('./pages/map/map.component').then((m) => m.MapComponent),
+      },
+      {
+        path: 'was',
+        loadComponent: () =>
+          import('./pages/was/was.component').then((m) => m.WasComponent),
+      },
     ],
   },
 ];
